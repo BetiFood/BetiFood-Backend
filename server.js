ab6fa704e597af5188035e7a306dad1012c298f8
Ali Fa <alifathy22a@gmail.com>
2025-06-26 13:07:27 +0300

    authentication of BetiFood

---
+++ server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+const port = 3000;
+
+const url = process.env.MONGODB_URL;
+const dbName = process.env.DB_NAME;
+
+async function connectToMongoDB() {
+  try {
+    const client = new MongoClient(url);
+    await client.connect();
+    await client.db(dbName).command({ ping: 1 });
+    console.log("Connected to MongoDB");
+    client.close();
+  } catch (err) {
+    console.error("Failed to connect to MongoDB:", err);
+    process.exit(1);
+  }
+}
+
+connectToMongoDB();
+
+app.use(express.json());
+app.use("/api/auth", require("./routes/auth"));
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+// Remove app.listen() for Vercel compatibility
+module.exports = app;
