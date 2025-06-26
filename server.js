f5c959f17ad3751aa8167aa87eb08cce379310fb
Ali Fa <alifathy22a@gmail.com>
2025-06-26 14:05:00 +0300

    finish the error

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const { MongoClient } = require("mongodb");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -10,9 +9,16 @@ const uri = process.env.MONGODB_URI;
 const dbName = process.env.DB_NAME;
 
 mongoose
-  .connect(process.env.MONGODB_URI, { dbName: process.env.DB_NAME })
+  .connect(process.env.MONGODB_URI, {
+    dbName: process.env.DB_NAME,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("Failed to connect to MongoDB:", err));
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+    process.exit(1);
+  });
 
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
