569b0474d2990d0d7221823250e5039e196e94d7
MahmoudFawzyHelmy <mahmoudfawzy.helmy@gmail.com>
2025-07-16 14:14:45 +0300

    charity crud

---
+++ routes/charity.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const charityController = require('../controllers/charityController');
+const checkRole = require("../middleware/roles");
+const auth = require("../middleware/auth");
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+router.post('/', auth, checkRole("admin"), upload.single('image'), charityController.createCharity);
+router.get('/', charityController.getAllCharities);
+router.put('/:id', auth, checkRole("admin"), upload.single('image'), charityController.updateCharity);
+router.delete('/:id', auth, checkRole("admin"), charityController.deleteCharity);
+
+module.exports = router;
